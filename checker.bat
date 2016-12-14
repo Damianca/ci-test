217efb34826d2bf3233afbc96ae817795ef67535
Damianca <cdamianc@hotmail.com>
2016-12-14 17:48:42 -0300

    Update checker.bat

--- checker.bat
+++ checker.bat
@@ -1,12 +1,14 @@
+setlocal ENABLEDELAYEDEXPANSION
 echo off
 findstr [@#$%] content.txt
 
 set result=%Errorlevel%
-echo result
+echo results below
 echo %result%
 
 IF /I "%result%" NEQ "1" (
     ECHO execution failed
+	EXIT /B 1
+) ELSE (
+	ECHO TEST PASSED
 )
-
-EXIT /B 1
